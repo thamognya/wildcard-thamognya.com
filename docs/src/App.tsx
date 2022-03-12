dbf33f523d8faab1e1c7d02f6b1058b64319878b
Thamognya Kodi <contact@thamognya.com>
2022-03-12 23:03:57 +0700

    website: all auto update

---
+++ docs/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>This is my website under construction. Hi</p>
+        <a href="https://www.thamognya.com">My Website</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
