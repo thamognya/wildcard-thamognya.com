13ac615a8ae240e01c9fe2e0a0e47a6b4f6b515d
Thamognya Kodi <contact@thamognya.com>
2022-03-18 07:54:31 +0700

    website: thamognya auto update

--- src/thamognya-src/src/App.tsx
+++ src/thamognya-src/src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+            Welcome to my website.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://blog.thamognya.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Blog
         </a>
       </header>
     </div>
