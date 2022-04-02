51eca18fc793815af3375d6e95595adbfafded38
Thamognya Kodi <contact@thamognya.com>
2022-04-02 18:27:13 +0700

    update

--- scripts/blog-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd ./src/blog-src/ && git add . && git commit -m 'blog.thamognya.com: auto-update' && git remote | xargs -L1 git push --all 
-
-sleep 2s
-
--- scripts/math-convert.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-query=$(printf "%s" "$*") && file_name=$(echo $query | sed 's/.\{3\}$//')
-html="html" && output="$file_name$html"
-pandoc $query -o $output
-xclip -sel c < $output
